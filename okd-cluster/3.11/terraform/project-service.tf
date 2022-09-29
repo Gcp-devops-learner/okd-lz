9009d90753999b02ccdead98457046ea2a2a9ce4
Avinash Jha <39315791+avinashkumar1289@users.noreply.github.com>
2022-09-29 20:55:52 +0530

    Update project-service.tf

--- okd-cluster/3.11/terraform/project-service.tf
+++ okd-cluster/3.11/terraform/project-service.tf
@@ -1,8 +1,8 @@
-module "project-services" {
-  source  = "terraform-google-modules/project-factory/google//modules/project_services"
-  version = "~> 13.0"
+# module "project-services" {
+#   source  = "terraform-google-modules/project-factory/google//modules/project_services"
+#   version = "~> 13.0"
 
-  project_id                  = var.project_id
+#   project_id                  = var.project_id
 
-  activate_apis = distinct(concat(var.activate_apis, ["billingbudgets.googleapis.com"]))
-}
+#   activate_apis = distinct(concat(var.activate_apis, ["billingbudgets.googleapis.com"]))
+# }
